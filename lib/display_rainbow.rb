0a53e2198a8dc9209c2301ee9d756f710e7329ed
Nichole C Cobb <nichole.c.cobb@gmail.com>
2020-07-13 03:01:30 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,5 @@
 # Write your #display_rainbow method here
-colors="{red,yello,orange,green,blue,indigo,violet}"
+colors="[red,yello,orange,green,blue,indigo,violet]"
 
 
 def display_rainbow(colors)
