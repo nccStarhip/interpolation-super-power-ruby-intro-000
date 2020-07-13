2a9b2f313a65b0e3108262c2ed339d1a0a465f2f
Nichole C Cobb <nichole.c.cobb@gmail.com>
2020-07-13 02:53:35 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -2,5 +2,6 @@
 colors = ["R: red","O: orange","Y: yellow","G: green","B: blue","I: indigo","V: violet"]
 
 def display_rainbow (colors)
-  puts colors
+  puts "colors: #{colors[0]}, colors: #{colors[1]}, colors: #{colors[2]}, colors: #{colors[3]}, colors: #{colors[4]}, colors: #{colors[5]}, colors: #{colors[6]}"
+  
 end
